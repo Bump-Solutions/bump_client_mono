5b94d27039501036e7ddcf602e9c5c0463ca69f5
harsnyi <harsanyitanulo@gmail.com>
2026-03-21 10:36:11 +0100

    Mobile app init

---
+++ apps/mobile/App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { PATHS } from "@bump/core/api";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>{PATHS.PRODUCT.LIKE_PRODUCT}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
